97c6f9c1a0808405ab1ad12356176b823c8bb342
Briana Bennett <bri.bennett89@gmail.com>
2017-06-23 13:27:10 -0500

    added scrolling about-me

--- main.js
+++ main.js
@@ -8,20 +8,31 @@ $(document).ready(function(){
 		$menu.removeClass('open');
 	});
 
-// 	$('.row').hover(
-// 	function() {
-//     $('#box-1').fadeOut(300, function() {
-//     	$('#box-b2').fadeIn(300);
-//     });
-//   },
-
-//   function() {
-//     $('#box-b2').fadeOut(300, function() {
-//     	$('#box-b1').fadeIn(300);
-//     });
-//   }
-// );
-
+	// scrolling words
+		var words = ["coder", "problem solver",  "team player", "full-stack developer"],
+			el = document.getElementById('scroll'),
+			word_counter = 0,
+			character_counter = 0;
+		function updateText()
+		{
+			let character = words[word_counter][character_counter];
+			if(character == ' '){
+				el.innerHTML = el.innerHTML + '&nbsp;';
+				character_counter++;
+			} else {
+				el.innerHTML = el.innerHTML+words[word_counter][character_counter++];
+			}
+			if(character_counter == words[word_counter].length +1)
+			{
+				word_counter++; 	//choose a different word
+				character_counter = 0;	//start over with the first character of the word
+				el.innerHTML = '';  //set the html to be blank
+				//if we're displaying the last word, go back to the first word
+				if(word_counter == words.length)
+					word_counter = 0;
+			}
+		}
+		setInterval(updateText,400);
 });
 
 
