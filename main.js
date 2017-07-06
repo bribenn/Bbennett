1ca91ca697a0dbd5375c9285338e7debd1368f73
Briana Bennett <bri.bennett89@gmail.com>
2017-07-06 14:39:56 -0500

    add smooth scrolling

--- main.js
+++ main.js
@@ -7,11 +7,47 @@ $(document).ready(function(){
 		$menu.removeClass('open');
 	});
 
+	// scroll smoothly to page section
+	// Select all links with hashes
+	$('a[href*="#"]').click(function(event) {
+	    // On-page links
+	    if (
+	      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+	      && 
+	      location.hostname == this.hostname
+	    ) {
+	      // Figure out element to scroll to
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	      // Does a scroll target exist?
+	      if (target.length) {
+	        // Only prevent default if animation is actually gonna happen
+	        event.preventDefault();
+	        $('html, body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000, function() {
+	          // Callback after animation
+	          // Must change focus!
+	          var $target = $(target);
+	          $target.focus();
+	          if ($target.is(":focus")) { // Checking if the target was focused
+	            return false;
+	          } else {
+	            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+	            $target.focus(); // Set focus again
+	          };
+	        });
+	      }
+	    }
+	  });
+	
+
 	// make project divs into links
 	$(".portfolio_img").click(function() {
 	  window.location = $(this).find("a").attr("href"); 
 	  return false;
 	});
+
 
 	// scrolling words
 		var words = ["coder", "problem solver",  "team player", "developer", "bit weird"],
